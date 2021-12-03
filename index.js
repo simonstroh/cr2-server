4ed1a0bfeb42ed7c9d7bcbabf190c62c70d995d7
Simon Stroh <simon_stroh@outlook.com>
2021-12-02 18:58:19 -0600

    Add express and GITHUB_OWNER

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const { Octokit } = require('@octokit/core');
 const app = express();
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 const path = require('path');
+const fs = require('fs');
 
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(express.json());
 app.post('/payload', async (req, res) => {
   const { body = {} } = req;
+  const { env } = process;
   if (body.hasOwnProperty('commits')) {
     const { after: ref } = body;
-    const owner = 'sceenius', repo = process.env.GITHUB_REPO;
+    const owner = env.GITHUB_OWNER, repo = env.GITHUB_REPO;
     const options = { owner, repo, ref };
     const { data: index } = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
       ...options,
@@ -25,6 +27,28 @@ app.post('/payload', async (req, res) => {
       ...options,
       path: '/css'
     });
+    (() => {
+      let { content, encoding, name } = index;
+      fs.writeFile(path.join(__dirname, 'build', name), content, {
+        encoding
+      });
+    })();
+    if (Array.isArray(js)) {
+      js.forEach(script => {
+        let { content, encoding, name } = script;
+        fs.writeFile(path.join(__dirname, 'build', 'js', name), content, {
+          encoding
+        });
+      });
+    }
+    if (Array.isArray(css)) {
+      css.forEach(style => {
+        let { content, encoding, name } = style;
+        fs.writeFile(path.join(__dirname, 'build', 'css', name), content, {
+          encoding
+        });
+      });
+    }
   }
 });
 
