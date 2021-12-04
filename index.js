fd9a43874a7abe4d9ddcf3d45d75eea78123c048
Simon Stroh <simon_stroh@outlook.com>
2021-12-03 19:35:55 -0600

    Add axios

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 const path = require('path');
 const http = require('http');
+const axios = require('axios');
 const { writeFile } = require('fs');
 
 app.use(express.static(path.join(__dirname, 'build')));
@@ -33,27 +34,36 @@ app.get('/outlets/:outlets/state', (req, res) => {
 });
 app.put('/outlets/:outlets/state', express.text(), (req, res) => {
   const { body, headers, method, params: { outlets } } = req;
-  const options = {
-    hostname: '201.182.226.142',
-    port: '5000',
-    path: `/restapi/relay/outlets/${outlets}/state/`,
-    method,
-    headers: {
-      authorization: headers.authorization,
-      'content-type': 'application/json'
-    }
-  };
-  const request = http.request(options, response => {
-    response.on('data', data => {
-      res.send(data);
+  const hostname = '201.182.226.142', port = '5000';
+  const path = `/restapi/relay/outlets/${outlets}/state/`;
+  const url = `http://${hostname}:${port}${path}`;
+  axios.put(url, body)
+    .then(response => res.send(response))
+    .catch(err => {
+      console.error(err);
+      res.status(500).end();
     });
-  });
-  request.on('error', err => {
-    console.error(err);
-    res.status(500).end();
-  });
-  request.write(new TextEncoder().encode(body));
-  request.end();
+  // const options = {
+  //   hostname: '201.182.226.142',
+  //   port: '5000',
+  //   path: `/restapi/relay/outlets/${outlets}/state/`,
+  //   method,
+  //   headers: {
+  //     authorization: headers.authorization,
+  //     'content-type': 'application/json'
+  //   }
+  // };
+  // const request = http.request(options, response => {
+  //   response.on('data', data => {
+  //     res.send(data);
+  //   });
+  // });
+  // request.on('error', err => {
+  //   console.error(err);
+  //   res.status(500).end();
+  // });
+  // request.write(new TextEncoder().encode(body));
+  // request.end();
 });
 app.post('/payload', express.json(), async (req, res) => {
   const { body = {} } = req;
